a8aa51d21934b1c822357d8e87ced8822e64f0f2
mxixo <msolivares100@gmail.com>
2026-08-08 00:03:02 -0700

    feat(runtime): export OpenClaw runtime adapter

---
+++ runtimes/openclaw/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const { OpenClawRuntimeProvider } = require("./openclaw-runtime-provider");
+const { SkillRunnerOpenClawExecutor } = require("./skill-runner-executor");
+
+module.exports = {
+  OpenClawRuntimeProvider,
+  SkillRunnerOpenClawExecutor,
+};
