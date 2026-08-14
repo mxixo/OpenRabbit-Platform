62a7fdc678d07d4ef63906f36e072ae245778766
mxixo <msolivares100@gmail.com>
2026-08-14 01:20:48 -0700

    Add durable underwriting state layer (#55)
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer
    
    * Add durable underwriting state layer

--- capabilities/real-estate/index.js
+++ capabilities/real-estate/index.js
@@ -2,10 +2,16 @@
 
 const manifest = require("./manifest");
 const commercialInvestmentWorkflow = require("./workflows/commercial-investment-workflow");
+const persistence = require("./persistence/state-repository");
+const { DurableUnderwritingService } = require("./persistence/durable-underwriting-service");
 
 module.exports = {
   manifest,
   workflows: {
     commercialInvestmentWorkflow,
+  },
+  persistence: {
+    ...persistence,
+    DurableUnderwritingService,
   },
 };
