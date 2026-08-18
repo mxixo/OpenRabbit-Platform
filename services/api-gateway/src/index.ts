b73373ef698b957a3610937aaa7c1fba6f3575ab
mxixo <msolivares100@gmail.com>
2026-08-17 23:41:38 -0700

    feat: export CRM adapter contracts

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -3,6 +3,7 @@ export * from "./platform-api.js";
 export * from "./today-api.js";
 export * from "./workspace-contracts.js";
 export * from "./workspace-api.js";
+export * from "./crm-adapter.js";
 export * from "./native-crm.js";
 export * from "./crm-api.js";
 export * from "./service.js";
