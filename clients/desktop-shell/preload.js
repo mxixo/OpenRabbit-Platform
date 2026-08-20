0a6cd1babfd17a09572d06fd0ee435c2d7e40c0f
mxixo <msolivares100@gmail.com>
2026-08-19 22:44:42 -0700

    Expose HubSpot OAuth connection to desktop renderer

--- clients/desktop-shell/preload.js
+++ clients/desktop-shell/preload.js
@@ -8,6 +8,7 @@ contextBridge.exposeInMainWorld('openRabbitDesktop', Object.freeze({
   startGoogleOAuth: (kind = 'gmail') => ipcRenderer.invoke('openrabbit:start-google-oauth', kind),
   connectGmail: () => ipcRenderer.invoke('openrabbit:start-google-oauth', 'gmail'),
   connectGoogleCalendar: () => ipcRenderer.invoke('openrabbit:start-google-oauth', 'calendar'),
+  connectHubSpot: () => ipcRenderer.invoke('openrabbit:start-hubspot-oauth'),
   getAgentProviderStatus: () => ipcRenderer.invoke('openrabbit:agent-provider-status'),
   connectChatGPT: () => ipcRenderer.invoke('openrabbit:connect-chatgpt'),
   agentChat: (messages) => ipcRenderer.invoke('openrabbit:agent-chat', messages)
