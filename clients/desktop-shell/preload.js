8802d7b430d97775013f6d6fcad05fb7774ef8dd
mxixo <msolivares100@gmail.com>
2026-08-19 19:36:17 -0700

    Expose restricted Google Maps browser key to workspace

--- clients/desktop-shell/preload.js
+++ clients/desktop-shell/preload.js
@@ -2,5 +2,6 @@ const { contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('openRabbitDesktop', Object.freeze({
   platform: process.platform,
-  desktop: true
+  desktop: true,
+  mapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY || process.env.OPENRABBIT_MAPS_BROWSER_KEY || ''
 }));
