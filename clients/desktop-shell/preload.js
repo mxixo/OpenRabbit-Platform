5f5186017526f4a4074612ad2ff765f9d1b19639
mxixo <msolivares100@gmail.com>
2026-08-19 19:39:09 -0700

    Expose desktop connection actions to OpenRabbit workspace

--- clients/desktop-shell/preload.js
+++ clients/desktop-shell/preload.js
@@ -1,7 +1,9 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('openRabbitDesktop', Object.freeze({
   platform: process.platform,
   desktop: true,
-  mapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY || process.env.OPENRABBIT_MAPS_BROWSER_KEY || ''
+  mapsBrowserKey: process.env.GOOGLE_MAPS_BROWSER_KEY || process.env.OPENRABBIT_MAPS_BROWSER_KEY || '',
+  getIntegrationStatus: () => ipcRenderer.invoke('openrabbit:integration-status'),
+  startGoogleOAuth: () => ipcRenderer.invoke('openrabbit:start-google-oauth')
 }));
