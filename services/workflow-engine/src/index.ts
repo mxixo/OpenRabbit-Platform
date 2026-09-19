00455f457597112d7cefb7ddc4396ea0d6be13f2
mxixo <msolivares100@gmail.com>
2026-09-19 16:52:20 -0700

    Export policy and action receipt primitives

--- services/workflow-engine/src/index.ts
+++ services/workflow-engine/src/index.ts
@@ -2,3 +2,5 @@ export * from "./contracts.js";
 export * from "./guardrails.js";
 export * from "./validator.js";
 export * from "./runner.js";
+export * from "./policy.js";
+export * from "./receipts.js";
