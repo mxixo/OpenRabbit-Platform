65d6e3163018e19c41f14a8296500fcc8bc040e9
mxixo <msolivares100@gmail.com>
2026-09-22 16:36:21 -0700

    Keep web entrypoint component-free

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,14 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import {StrictMode} from 'react';
-import {BrowserRouter,useLocation} from 'react-router-dom';
-import App from './App.jsx';
-import OnboardingPage from './OnboardingPage.jsx';
+import {BrowserRouter} from 'react-router-dom';
+import RootSurface from './RootSurface.jsx';
 import './ConnectionsPage.jsx';
 import './styles.css';
-
-function RootSurface(){
-  const location=useLocation();
-  return location.pathname==='/onboarding'?<OnboardingPage/>:<App/>;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(<StrictMode><BrowserRouter><RootSurface/></BrowserRouter></StrictMode>);
